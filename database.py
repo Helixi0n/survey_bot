6352f70976fb4aa87b3af0d76539914c762a7eb3
Helixi0n <111045175+Helixi0n@users.noreply.github.com>
2025-04-27 12:49:50 +0700

    Add files via upload

--- database.py
+++ database.py
@@ -1,31 +1,48 @@
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, Text, String, JSON, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine('sqlite:///school.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+BaseModel = declarative_base()
+
+class User(BaseModel):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+
+    survey = relationship('Survey', back_populates='user_id')
+
+class Survey(BaseModel):
+    __tablename__ = 'survey'
+
+    id = Column(Integer, primary_key=True)
+    passed = Column(Integer)
+    title = Column(String)
+    description = Column(Text)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    
+    user = relationship('User', back_populates='survey')
+    questions = relationship('Question', back_populates='survey')
+
+class Question(BaseModel):
+    __tablename__ = 'question'
+
+    id = Column(Integer, primary_key=True)
+    survey_id = Column(Integer, ForeignKey('survey.id'))
+    question_title = Column(Text)
+    answers_data = Column(JSON, default={})
+
+    survey = relationship('Survey', back_populates='questions')
 
 def init_db():
-    connection = sqlite3.connect("data.db")
-    cursor = connection.execute(
-        """
-	CREATE TABLE IF NOT EXISTS survey (
-		id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-		title TEXT NOT NULL,
-		description TEXT,
-        );
-
-    CREATE TABLE IF NOT EXISTS questions (
-        quesion_id INTEGER PRYMARY KEY,
-        survey_id INTEGER,
-        question_text TEXT NOT NULL,
-        yes INTEGER,
-        no INTEGER,
-        answer INTEGER
-        );
-	"""
-    )
-
-    connection.commit()
-    connection.close()
-
+    engine = create_engine("sqlite:///data.db")
+    BaseModel.metadata.create_all(engine)
 
 def get_connection():
-    return sqlite3.connect("data.db")
+    engine = create_engine('sqlite:///data.db')
+    return sessionmaker(bind=engine)
